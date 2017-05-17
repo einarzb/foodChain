2429bde3a6ea3f81e02fdbc0744f0d33f6d05a1c
einarzb <einarzb@gmail.com>
2017-05-17 13:20:59 +0300

    got lat and lng by search name result and save them in vars

--- public/js/mainController.js
+++ public/js/mainController.js
@@ -2,14 +2,30 @@ app.controller('mainController', function($scope, factory, geolocation) {
 $scope.stores = factory.stores;
 $scope.currentLocation;
 
-$scope.getLocation = function(currentLocation){
-  console.log(currentLocation);
-  //now I need to send current location to factory and there do the math
-}
+// $scope.getLocation = function(currentLocation){
+//   console.log(currentLocation);
+//   //now I need to send current location to factory and there do the math
+// }
+
+function GetLatlong(currentLocation)
+    {
+      alert("yoyoyo")
+        var geocoder = new google.maps.Geocoder();
+        var address = document.getElementById('textboxid').value;
+
+        geocoder.geocode({ 'address': address }, function (results, status) {
+
+            if (status == google.maps.GeocoderStatus.OK) {
+                var latitude = results[0].geometry.location.lat();
+                var longitude = results[0].geometry.location.lng();
+
+            }
+        });
+  }
 
 geolocation.getLocation().then(function(data){
       $scope.coords = {lat:data.coords.latitude, long:data.coords.longitude};
     });
 
-    
+
 });
